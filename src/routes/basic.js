d909a03183733e2db6908a7f51e3ccb733ff7111
MaoCorreag <63886571+MaoCorreag@users.noreply.github.com>
2021-05-03 22:50:15 -0500

    ver1.2.1

--- src/routes/basic.js
+++ src/routes/basic.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const pool= require('../database');
 
-router.get('/add',(req,res)=>{
-      res.render('basic/add');
+router.get('/add',async(req,res)=>{
+   // res.send('Oh yeah');
+   const usua = await pool.query('select * from `usuarios`');
+   res.render('basic/add',{usua});
+});
+router.post('/add',async(req,res)=>{
+  console.log(req.body);
+  await pool.query('insert into usuarios set ?', [req.body]);
+  res.redirect('/basic/add');
 });
 
 module.exports= router;
--- src/routes/pro.js
+++ src/routes/pro.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const pool= require('../database');
 
-router.get('/add',(req,res)=>{
-      res.render('pro/add');
+router.get('/add',async(req,res)=>{
+   // res.send('Oh yeah');
+   const usua = await pool.query('select * from `usuarios`');
+   res.render('pro/add',{usua});
+});
+router.post('/add',async(req,res)=>{
+  console.log(req.body);
+  await pool.query('insert into usuarios set ?', [req.body]);
+  res.redirect('/pro/add');
 });
 
 module.exports= router;
