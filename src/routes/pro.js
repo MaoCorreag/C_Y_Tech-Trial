554d9855356662c1b684b539bf2ed0bddc1f6072
MaoCorreag <63886571+MaoCorreag@users.noreply.github.com>
2021-05-13 16:53:39 -0500

    ver1.3

--- src/routes/pro.js
+++ src/routes/pro.js
@@ -7,10 +7,37 @@ router.get('/add',async(req,res)=>{
    const usua = await pool.query('select * from `usuarios`');
    res.render('pro/add',{usua});
 });
+
 router.post('/add',async(req,res)=>{
   console.log(req.body);
   await pool.query('insert into usuarios set ?', [req.body]);
   res.redirect('/pro/add');
 });
+
+router.get('/delete/:id_user',async(req,res)=>{
+  const {id_user}= req.params;
+  const usua= await pool.query('delete from usuarios where id_user=?',[id_user]);
+  res.redirect('/pro/add');
+});
+
+
+router.get('/update',async(req,res)=>{
+  // res.send('Oh yeah');
+  const usua = await pool.query('select * from `usuarios`');
+  res.render('pro/update',{usua});
+});
+
+router.post('/update', async(req,res)=>{
+  // res.send('Oh yeah');
+  console.log(req.body);
+  const usua1 = await pool.query('select * from `usuarios`');
+  await pool.query('update usuarios set nombres=?, where id_user=?',
+  [req.body.nombres, req.body.id_user]);
+  res.redirect('/pro/update');
+});
+
+ 
+
+
 
 module.exports= router;
